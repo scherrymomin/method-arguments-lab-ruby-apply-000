47be27588018392839079a4c7d71804321fc3e23
thoughtful-cookie-2669 <scherezade.momin@gmail.com>
2020-07-23 23:33:32 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,11 @@
+ledef introduction(name)
+  puts "Hi, my name is #{name}"
+end
+
+introduction("scherry")
+
+def introduction_with_language(name, language)
+  puts "hi my name is #{name} and I am learning to program in #{language}"
+end
+
+introduction_with_language("scherry", "ruby")
